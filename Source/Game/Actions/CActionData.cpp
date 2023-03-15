3e9ddf360330cef98ed5f01b09ba1f0f63ad6d39
포도맛 버섯 <skacjstk0818@gmail.com>
2023-03-15 21:46:37 +0900

    [2023.03.15] SaveGame 간단테스트
    SaveGame은 노가다만 하면 된다...

--- Source/Game/Actions/CActionData.cpp
+++ Source/Game/Actions/CActionData.cpp
@@ -32,7 +32,7 @@ void UCActionData::BeginPlay(ACharacter* InOwnerCharacter, UCActionObjectContain
 		// https://minusi.tistory.com/entry/%EC%96%B8%EB%A6%AC%EC%96%BC-%EC%98%A4%EB%B8%8C%EC%A0%9D%ED%8A%B8Unreal-Object-Outer
 		if (!!AttachmentStatusComp)
 		{
-			Attachment->SetAttachmentStatusComponent(AttachmentStatusComp);	// GetOwner()로 가져오면 되는데 혹시몰라서
+			Attachment->SetAttachmentStatusComponent(AttachmentStatusComp);	// GetOwner()로 가져오면 되는데 혹시몰라서			
 			AttachmentStatusComp->InitStatus(Attachment->AttachmentStatusData);
 		}
 	}
@@ -86,10 +86,13 @@ void UCActionData::BeginPlay(ACharacter* InOwnerCharacter, UCActionObjectContain
 
 	}//end if
 	// 최종적으로 UCActionData의 BeginPlay를 호출한 곳의 OutObject에 완성된 결과를 반환. 현재는 CActionComponent::BeginPlay() 에서 호출한다.
+
 	*OutObject = NewObject<UCActionObjectContainer>(InOwnerCharacter);	// 시도: ActionData 의 Owner는 Character
 	(*OutObject)->Attachment = Attachment;
 	(*OutObject)->Equipment = Equipment;
 	(*OutObject)->DoAction = DoAction;		// Friend 먹여놔서 된다. 
+
+	
 	(*OutObject)->EquipmentColor = EquipmentColor;
 
 }
